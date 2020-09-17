26cf846c71d31f2e0e0ed42e6689bd3cc8785c21
Hitesh Patel <hbp976@gmail.com>
2020-09-17 12:57:05 -0600

    added new coponent hello-warren

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import HelloWorld from './components/HelloWorld';
 import HelloStudent from './components/HelloStudent';
+import HelloWarren from './components/HelloWarren';
 
 
 
@@ -12,6 +13,7 @@ ReactDOM.render(
     <HelloWorld />
     <HelloStudent name="Eboka" />
     <HelloStudent name="Atinder" />
+    <HelloWarren />
   </React.StrictMode>,
   document.getElementById('root')
 );
